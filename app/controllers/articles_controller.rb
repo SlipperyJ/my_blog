d3f988b8b7e2c46d3d16ae202e868b569deacfb1
SlipperyJ <rossghamilton@hotmail.co.uk>
2015-01-08 23:13:15 +0000

    Complete app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,24 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(articles_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render "new"
+    end
+  end
+
+  private
+    def articles_params
+      params.require(:article).permit(:title, :content)
+    end
 end
